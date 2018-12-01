875f6d683263144c4d5f89cce4e803bfd91b5ac2
wxx <921623221@qq.com>
2018-12-01 10:35:33 +0800

    managersManagement页面查找功能

--- src/src/main/java/com/gxu/tbvp/controller/ManagerController.java
+++ src/src/main/java/com/gxu/tbvp/controller/ManagerController.java
@@ -1,19 +1,26 @@
 package com.gxu.tbvp.controller;
 
+import com.github.pagehelper.PageInfo;
 import com.gxu.tbvp.domain.Manager;
+
 import com.gxu.tbvp.service.ManagerService;
 import com.gxu.tbvp.utils.PasswordHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
-@RequestMapping("/manager")
+@RestController
+@RequestMapping("/managers")
 public class ManagerController {
     @Resource
     private ManagerService managerService;
@@ -41,5 +48,69 @@ public class ManagerController {
             return "404";
         }
     }
+
+    @RequestMapping
+    public Map<String,Object> getAll(Manager manager, String draw,
+                                     @RequestParam(required = false, defaultValue = "1") int start,
+                                     @RequestParam(required = false, defaultValue = "10") int length){
+        Map<String,Object> map = new HashMap<>();
+        PageInfo<Manager> pageInfo = managerService.selectByPage(manager, start, length);
+        System.out.println("pageInfo.getTotal():"+pageInfo.getTotal());
+        map.put("draw",draw);
+        map.put("recordsTotal",pageInfo.getTotal());
+        map.put("recordsFiltered",pageInfo.getTotal());
+        map.put("data", pageInfo.getList());
+        return map;
+    }
+
+
+    /**
+     * 保存用户角色
+     * @param managerRole 用户角色
+     *  	  此处获取的参数的角色id是以 “,” 分隔的字符串
+     * @return
+     */
+//    @RequestMapping("/saveUserRoles")
+//    public String saveUserRoles(ManagerRole managerRole){
+//        if(StringUtils.isEmpty(managerRole.getRoleid()))
+//            return "error";
+//        try {
+//            managerRoleService.add
+//            userRoleService.addUserRole(userRole);
+//            return "success";
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return "fail";
+//        }
+//    }
+
+    @RequestMapping(value = "/add")
+    public String add(Manager manager) {
+        Manager u = managerService.selectByUsername(manager.getUsername());
+        if(u != null)
+            return "error";
+        try {
+            manager.setEnable(1);
+            PasswordHelper passwordHelper = new PasswordHelper();
+            passwordHelper.encryptPassword(manager);
+            managerService.save(manager);
+            return "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "fail";
+        }
+    }
+
+    @RequestMapping(value = "/delete")
+    public String delete(Integer id){
+        try{
+            managerService.delUser(id);
+            return "success";
+        }catch (Exception e){
+            e.printStackTrace();
+            return "fail";
+        }
+    }
+
 
 }
